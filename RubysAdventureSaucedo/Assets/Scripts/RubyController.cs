d36b335253ab3ddf24e3971952127dd53474d7da
MathSauc3 <732584@student.dusd.net>
2022-11-01 10:46:51 -0700

    Save scene
    created and save scene

--- RubysAdventureSaucedo/Assets/Scripts/RubyController.cs
+++ RubysAdventureSaucedo/Assets/Scripts/RubyController.cs
@@ -17,8 +17,8 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal * Time.deltaTime;
-        position.y = position.y + 0.1f * vertical * Time.deltaTime;
+        position.x = position.x + 2f * horizontal * Time.deltaTime;
+        position.y = position.y + 2f * vertical * Time.deltaTime;
         transform.position = position; 
     }
 }
