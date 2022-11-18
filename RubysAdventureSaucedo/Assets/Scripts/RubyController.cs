edf3a0360cf5755d6b1cbedbc2640e9dd100958f
MathSauc3 <732584@student.dusd.net>
2022-11-18 11:19:18 -0800

    Added Collisions
    Added collisons to characters and objects

--- RubysAdventureSaucedo/Assets/Scripts/RubyController.cs
+++ RubysAdventureSaucedo/Assets/Scripts/RubyController.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
-    
+
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 10f * horizontal * Time.deltaTime;
-        position.y = position.y + 10f * vertical * Time.deltaTime;
-        transform.position = position; 
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + (10f * horizontal * Time.deltaTime);
+        position.y = position.y + (10f * vertical * Time.deltaTime);
+
+        rigidbody2d.MovePosition(position);
+
+
     }
 }
